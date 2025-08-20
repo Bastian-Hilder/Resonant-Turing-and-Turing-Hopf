42411a8b82d7695761093f1511d9fe2ec51f59a5
Bastian Hilder <140402239+Bastian-Hilder@users.noreply.github.com>
2025-08-20 21:44:16 +0200

    Initial commit

---
+++ Dynamics on invariant set/Periodic_vs_QuasiPeriodic.m
@@ -0,0 +1,72 @@
+%% numerical simulation of a simple example system for (7.8)
+% We cosider the system
+%   \partial_t A = \eps^2*(A - (1+5i) A|A|^2 + (1+5i) A|B_o|^2)
+%                           + \eps^4 B_o |A|^4 e^{i t}
+%   \partial_t B_o = \eps^2*(B_o - B_o|B_o|^2 - (2-i) B_o|A^2|) 
+%                           + \eps^4*A|A|^5 e^{-i t}
+
+%% periodic
+
+eps = 0.1;
+Tend = 100000;
+omegaA = 5;
+
+vecField = @(t,X) [eps^2*X(1).*(1 - (1 + 5*1i)*abs(X(1)).^2 + (1+5*1i)*abs(X(2)).^2) + eps^4*X(2).*abs(X(1)).^4.*exp(1i * t);...
+                    eps^2*X(2).*(1 - abs(X(2)).^2 - (2-1i)*abs(X(1)).^2) + eps^4*X(1).*abs(X(1)).^4.*exp(-1i * t)];
+
+Xinit = [1,0];
+[t,x] = ode45(vecField,[0,Tend],Xinit);
+
+figure;
+plot(angle(exp(1i*t)),angle(x(:,1).*exp(-1i*omegaA*eps^2*t)),'.');
+xlabel("$\theta$","FontSize",16,"Interpreter","latex");
+ylabel("$\phi_A$","FontSize",16,"Interpreter","latex");
+title("$\varepsilon = 0.1$","FontSize",16,"Interpreter","latex");
+ylim([-pi,pi]);
+xlim([-pi,pi]);
+saveas(gcf,'periodic.png')
+
+figure;
+subplot(2,1,1);
+plot(t,abs(x(:,1)));
+xlabel("$t$","Interpreter","latex");
+ylabel("$|A|$","Interpreter","latex");
+subplot(2,1,2);
+plot(t,abs(x(:,2)));
+xlabel("$t$","Interpreter","latex");
+ylabel("$|B|$","Interpreter","latex");
+saveas(gcf,'amplitudes-eps01.png');
+
+
+%% quasi-periodic
+
+eps = 0.09;
+Tend = 100000;
+omegaA = 5;
+
+vecField = @(t,X) [eps^2*X(1).*(1 - (1 + 5*1i)*abs(X(1)).^2 + (1+5*1i)*abs(X(2)).^2) + eps^4*X(2).*abs(X(1)).^4.*exp(1i * t);...
+                    eps^2*X(2).*(1 - abs(X(2)).^2 - (2-1i)*abs(X(1)).^2) + eps^4*X(1).*abs(X(1)).^4.*exp(-1i * t)];
+
+
+Xinit = [1,0];
+[t,x] = ode45(vecField,[0,Tend],Xinit);
+
+figure;
+plot(angle(exp(1i*t)),angle(x(:,1).*exp(-1i*omegaA*eps^2*t)),'.');
+xlabel("$\theta$","FontSize",16,"Interpreter","latex");
+ylabel("$\phi_A$","FontSize",16,"Interpreter","latex");
+title("$\varepsilon = 0.09$","FontSize",16,"Interpreter","latex");
+ylim([-pi,pi]);
+xlim([-pi,pi]);
+saveas(gcf,"quasiperiodic.png");
+
+figure;
+subplot(2,1,1);
+plot(t,abs(x(:,1)));
+xlabel("$t$","Interpreter","latex");
+ylabel("$|A|$","Interpreter","latex");
+subplot(2,1,2);
+plot(t,abs(x(:,2)));
+xlabel("$t$","Interpreter","latex");
+ylabel("$|B|$","Interpreter","latex");
+saveas(gcf,'amplitudes-eps009.png');
---
+++ Pattern-forming fronts/fronts.m
@@ -0,0 +1,590 @@
+%% script to plot the pattern interfaces found close to a 1:1 resonant Turing and Turing–Hopf instability
+
+close all
+
+makevideo = 0;
+
+%% coefficients
+tildec = 0.5;
+gammaA = -2;
+gammaB = -2;
+
+gamma1i = 1;
+gamma2i = 1;
+gamma7i = 1;
+gamma8i = 1;
+
+Tend = 15;
+
+%% vecfield
+
+omegaA = gamma1i;
+omegaB = gamma8i;
+
+% X = (rA, rB, phiA, phiB)
+vecfield = @(~,X) [-X(1) + X(1).^3 - gammaA*X(1).*X(2).^2;...
+                    (1/tildec)*(X(2) - X(2).^3 + gammaB*X(2).*X(1).^2);...
+                    -omegaA + gamma1i*X(1).^2 + gamma2i*X(2).^2;...
+                    (1/tildec)*(-omegaB + gamma7i*X(1).^2 + gamma8i*X(2).^2)];
+
+
+%% Front Turing–Hopf to Superposition
+
+% solve ode
+Xinit = [1e-5,1,0,0];
+
+[t,X] = ode45(vecfield,[-Tend,Tend],Xinit);
+% figure;
+% plot(X(:,1),X(:,2));
+% 
+% pause();
+
+% plot solution
+
+% interpolate solution to uniform grid
+xiGrid = linspace(-Tend,Tend,10000);
+rA = interp1(t,X(:,1),xiGrid);
+rB = interp1(t,X(:,2),xiGrid);
+
+PhiA = interp1(t,X(:,3),xiGrid);
+PhiB = interp1(t,X(:,4),xiGrid);
+
+% figure;
+% plot(xiGrid,rA,xiGrid,rB);
+% 
+% figure;
+% plot(xiGrid,PhiA,xiGrid,PhiB);
+% 
+% pause();
+
+% full solution
+
+eps = 0.2;
+c0 = 1;
+cp = 0.5;
+
+u = @(x,time) 2*eps*interp1(t,X(:,1),eps^2*(x-c0*time)).*cos(x+interp1(t,X(:,3),eps^2*(x-c0*time))+eps^2*omegaA*time);
+v = @(x,time) 2*eps*interp1(t,X(:,2),eps^2*(x-c0*time)).*cos(x-cp*time+interp1(t,X(:,4),eps^2*(x-c0*time))+eps^2*omegaB*time);
+
+
+xgrid = linspace(0,50,1000);
+tgrid = linspace(0,300,1000);
+
+% video
+if makevideo
+    for ii = 1:numel(tgrid)
+        subplot(2,1,1);
+        plot(xgrid,u(xgrid,tgrid(ii)));
+        title("$u$","Interpreter","latex");
+        xlim([0,50]);
+        ylim([-0.5,0.5]);
+    
+        subplot(2,1,2);
+        plot(xgrid,v(xgrid,tgrid(ii)));
+        title("$v$","Interpreter","latex");
+        xlim([0,50]);
+        ylim([-0.5,0.5]);
+        pause(0.0001)
+    end
+end
+
+
+
+% space-time plot (top-down view)
+
+figure
+[Xmesh,T] = meshgrid(linspace(0,50,1000),linspace(0,300,1000));
+U = u(Xmesh,T);
+V = v(Xmesh,T);
+
+hold on
+s = pcolor(Xmesh,T,U);
+grid off;
+s.EdgeColor="none";
+colormap(flipud(gray))
+xlabel("$\tilde{\xi}$","FontSize",20,"Interpreter","latex")
+ylabel("$t$","FontSize",20,"Interpreter","latex")
+title("$u$","FontSize",20,"Interpreter","latex")
+xlim([0,50])
+ylim([0,300])
+pbaspect([4/2 1 1])
+
+set(gcf,"Position",[100, 100, 800, 420])
+
+saveas(gcf,"u-TH-to-Superpos.png")
+
+figure
+
+hold on
+s = pcolor(Xmesh,T,V);
+grid off;
+s.EdgeColor="none";
+colormap(flipud(gray))
+xlabel("$\tilde{\xi}$","FontSize",20,"Interpreter","latex")
+ylabel("$t$","FontSize",20,"Interpreter","latex")
+title("$v$","FontSize",20,"Interpreter","latex")
+xlim([0,50])
+ylim([0,300])
+pbaspect([4/2 1 1])
+
+set(gcf,"Position",[100, 100, 800, 420])
+
+saveas(gcf,"v-TH-to-Superpos.png")
+
+
+%% Front Turing to Trivial
+
+timeEnd = 350;
+Xend = 50;
+
+% solve ode
+Xinit = [1-1e-7,0,0,0];
+
+[t,X] = ode45(vecfield,[-Tend,Tend],Xinit);
+% figure;
+% plot(X(:,1),X(:,2));
+% 
+% pause();
+
+% plot solution
+
+% interpolate solution to uniform grid
+xiGrid = linspace(-Tend,Tend,10000);
+rA = interp1(t,X(:,1),xiGrid);
+rB = interp1(t,X(:,2),xiGrid);
+
+PhiA = interp1(t,X(:,3),xiGrid);
+PhiB = interp1(t,X(:,4),xiGrid);
+
+% figure;
+% plot(xiGrid,rA,xiGrid,rB);
+% 
+% figure;
+% plot(xiGrid,PhiA,xiGrid,PhiB);
+% 
+% pause();
+
+% full solution
+
+eps = 0.2;
+c0 = 1;
+cp = 0.5;
+
+u = @(x,time) 2*eps*interp1(t,X(:,1),eps^2*(x-c0*time)).*cos(x+interp1(t,X(:,3),eps^2*(x-c0*time))+eps^2*omegaA*time);
+v = @(x,time) 2*eps*interp1(t,X(:,2),eps^2*(x-c0*time)).*cos(x-cp*time+interp1(t,X(:,4),eps^2*(x-c0*time))+eps^2*omegaB*time);
+
+
+xgrid = linspace(0,Xend,1000);
+tgrid = linspace(0,timeEnd,1000);
+
+% video
+if makevideo
+    for ii = 1:numel(tgrid)
+        subplot(2,1,1);
+        plot(xgrid,u(xgrid,tgrid(ii)));
+        title("$u$","Interpreter","latex");
+        xlim([0,50]);
+        ylim([-0.5,0.5]);
+    
+        subplot(2,1,2);
+        plot(xgrid,v(xgrid,tgrid(ii)));
+        title("$v$","Interpreter","latex");
+        xlim([0,Xend]);
+        ylim([-0.5,0.5]);
+        pause(0.0001)
+    end
+end
+
+
+
+% space-time plot (top-down view)
+
+figure
+[Xmesh,T] = meshgrid(linspace(0,Xend,1000),linspace(0,timeEnd,1000));
+U = u(Xmesh,T);
+V = v(Xmesh,T);
+
+hold on
+s = pcolor(Xmesh,T,U);
+grid off;
+s.EdgeColor="none";
+colormap(flipud(gray))
+xlabel("$\tilde{\xi}$","FontSize",20,"Interpreter","latex")
+ylabel("$t$","FontSize",20,"Interpreter","latex")
+title("$u$","FontSize",20,"Interpreter","latex")
+xlim([0,Xend])
+ylim([0,timeEnd])
+pbaspect([4/2 1 1])
+
+set(gcf,"Position",[100, 100, 800, 420])
+
+saveas(gcf,"u-T-to-trivial.png")
+
+figure
+
+hold on
+s = pcolor(Xmesh,T,V);
+grid off;
+s.EdgeColor="none";
+colormap(flipud(gray))
+xlabel("$\tilde{\xi}$","FontSize",20,"Interpreter","latex")
+ylabel("$t$","FontSize",20,"Interpreter","latex")
+title("$v$","FontSize",20,"Interpreter","latex")
+xlim([0,Xend])
+ylim([0,timeEnd])
+pbaspect([4/2 1 1])
+
+set(gcf,"Position",[100, 100, 800, 420])
+
+saveas(gcf,"v-T-to-trivial.png")
+
+%% Front Turing–Hopf to Trivial
+
+% change speed and update vectorfield
+tildec = -0.5;
+
+% X = (rA, rB, phiA, phiB)
+vecfield = @(~,X) [-X(1) + X(1).^3 - gammaA*X(1).*X(2).^2;...
+                    (1/tildec)*(X(2) - X(2).^3 + gammaB*X(2).*X(1).^2);...
+                    -omegaA + gamma1i*X(1).^2 + gamma2i*X(2).^2;...
+                    (1/tildec)*(-omegaB + gamma7i*X(1).^2 + gamma8i*X(2).^2)];
+
+timeEnd = 350;
+Xend = 50;
+
+% solve ode
+Xinit = [0,1-1e-15,0,0];
+
+[t,X] = ode45(vecfield,[-Tend,Tend],Xinit);
+% figure;
+% plot(X(:,1),X(:,2));
+
+% plot solution
+
+% interpolate solution to uniform grid
+xiGrid = linspace(-Tend,Tend,10000);
+rA = interp1(t,X(:,1),xiGrid);
+rB = interp1(t,X(:,2),xiGrid);
+
+PhiA = interp1(t,X(:,3),xiGrid);
+PhiB = interp1(t,X(:,4),xiGrid);
+
+% figure;
+% plot(xiGrid,rA,xiGrid,rB);
+% 
+% figure;
+% plot(xiGrid,PhiA,xiGrid,PhiB);
+% 
+% pause();
+
+% full solution
+
+eps = 0.2;
+c0 = 1;
+cp = 0.5;
+
+u = @(x,time) 2*eps*interp1(t,X(:,1),eps^2*(x-c0*time)).*cos(x+interp1(t,X(:,3),eps^2*(x-c0*time))+eps^2*omegaA*time);
+v = @(x,time) 2*eps*interp1(t,X(:,2),eps^2*(x-c0*time)).*cos(x-cp*time+interp1(t,X(:,4),eps^2*(x-c0*time))+eps^2*omegaB*time);
+
+
+xgrid = linspace(0,Xend,1000);
+tgrid = linspace(0,timeEnd,1000);
+
+% video
+if makevideo
+    for ii = 1:numel(tgrid)
+        subplot(2,1,1);
+        plot(xgrid,u(xgrid,tgrid(ii)));
+        title("$u$","Interpreter","latex");
+        xlim([0,50]);
+        ylim([-0.5,0.5]);
+    
+        subplot(2,1,2);
+        plot(xgrid,v(xgrid,tgrid(ii)));
+        title("$v$","Interpreter","latex");
+        xlim([0,Xend]);
+        ylim([-0.5,0.5]);
+        pause(0.0001)
+    end
+end
+
+
+
+% space-time plot (top-down view)
+
+figure
+[Xmesh,T] = meshgrid(linspace(0,Xend,1000),linspace(0,timeEnd,1000));
+U = u(Xmesh,T);
+V = v(Xmesh,T);
+
+hold on
+s = pcolor(Xmesh,T,U);
+grid off;
+s.EdgeColor="none";
+colormap(flipud(gray))
+xlabel("$\tilde{\xi}$","FontSize",20,"Interpreter","latex")
+ylabel("$t$","FontSize",20,"Interpreter","latex")
+title("$u$","FontSize",20,"Interpreter","latex")
+xlim([0,Xend])
+ylim([0,timeEnd])
+pbaspect([4/2 1 1])
+
+set(gcf,"Position",[100, 100, 800, 420])
+
+saveas(gcf,"u-TH-to-trivial.png")
+
+figure
+
+hold on
+s = pcolor(Xmesh,T,V);
+grid off;
+s.EdgeColor="none";
+colormap(flipud(gray))
+xlabel("$\tilde{\xi}$","FontSize",20,"Interpreter","latex")
+ylabel("$t$","FontSize",20,"Interpreter","latex")
+title("$v$","FontSize",20,"Interpreter","latex")
+xlim([0,Xend])
+ylim([0,timeEnd])
+pbaspect([4/2 1 1])
+
+set(gcf,"Position",[100, 100, 800, 420])
+
+saveas(gcf,"v-TH-to-trivial.png")
+
+
+%% Front Turing to Turing–Hopf
+
+% change speed and update vectorfield
+tildec = 0.5;
+gammaA = 1;
+gammaB = 2;
+
+% X = (rA, rB, phiA, phiB)
+vecfield = @(~,X) [-X(1) + X(1).^3 - gammaA*X(1).*X(2).^2;...
+                    (1/tildec)*(X(2) - X(2).^3 + gammaB*X(2).*X(1).^2);...
+                    -omegaA + gamma1i*X(1).^2 + gamma2i*X(2).^2;...
+                    (1/tildec)*(-omegaB + gamma7i*X(1).^2 + gamma8i*X(2).^2)];
+
+timeEnd = 350;
+Xend = 50;
+
+% solve ode
+Xinit = [1,1e-15,0,0];
+
+[t,X] = ode45(vecfield,[-Tend,Tend],Xinit);
+% figure;
+% plot(X(:,1),X(:,2));
+
+% plot solution
+
+% interpolate solution to uniform grid
+xiGrid = linspace(-Tend,Tend,10000);
+rA = interp1(t,X(:,1),xiGrid);
+rB = interp1(t,X(:,2),xiGrid);
+
+PhiA = interp1(t,X(:,3),xiGrid);
+PhiB = interp1(t,X(:,4),xiGrid);
+
+% figure;
+% plot(xiGrid,rA,xiGrid,rB);
+% 
+% figure;
+% plot(xiGrid,PhiA,xiGrid,PhiB);
+% 
+% pause();
+
+% full solution
+
+eps = 0.2;
+c0 = 1;
+cp = 0.5;
+
+u = @(x,time) 2*eps*interp1(t,X(:,1),eps^2*(x-c0*time)).*cos(x+interp1(t,X(:,3),eps^2*(x-c0*time))+eps^2*omegaA*time);
+v = @(x,time) 2*eps*interp1(t,X(:,2),eps^2*(x-c0*time)).*cos(x-cp*time+interp1(t,X(:,4),eps^2*(x-c0*time))+eps^2*omegaB*time);
+
+
+xgrid = linspace(0,Xend,1000);
+tgrid = linspace(0,timeEnd,1000);
+
+% video
+if makevideo
+    for ii = 1:numel(tgrid)
+        subplot(2,1,1);
+        plot(xgrid,u(xgrid,tgrid(ii)));
+        title("$u$","Interpreter","latex");
+        xlim([0,50]);
+        ylim([-0.5,0.5]);
+    
+        subplot(2,1,2);
+        plot(xgrid,v(xgrid,tgrid(ii)));
+        title("$v$","Interpreter","latex");
+        xlim([0,Xend]);
+        ylim([-0.5,0.5]);
+        pause(0.0001)
+    end
+end
+
+
+
+% space-time plot (top-down view)
+
+figure
+[Xmesh,T] = meshgrid(linspace(0,Xend,1000),linspace(0,timeEnd,1000));
+U = u(Xmesh,T);
+V = v(Xmesh,T);
+
+hold on
+s = pcolor(Xmesh,T,U);
+grid off;
+s.EdgeColor="none";
+colormap(flipud(gray))
+xlabel("$\tilde{\xi}$","FontSize",20,"Interpreter","latex")
+ylabel("$t$","FontSize",20,"Interpreter","latex")
+title("$u$","FontSize",20,"Interpreter","latex")
+xlim([0,Xend])
+ylim([0,timeEnd])
+pbaspect([4/2 1 1])
+
+set(gcf,"Position",[100, 100, 800, 420])
+
+saveas(gcf,"u-T-to-TH.png")
+
+figure
+
+hold on
+s = pcolor(Xmesh,T,V);
+grid off;
+s.EdgeColor="none";
+colormap(flipud(gray))
+xlabel("$\tilde{\xi}$","FontSize",20,"Interpreter","latex")
+ylabel("$t$","FontSize",20,"Interpreter","latex")
+title("$v$","FontSize",20,"Interpreter","latex")
+xlim([0,Xend])
+ylim([0,timeEnd])
+pbaspect([4/2 1 1])
+
+set(gcf,"Position",[100, 100, 800, 420])
+
+saveas(gcf,"v-T-to-TH.png")
+
+
+%% Front Superposition to Turing
+
+% change speed and update vectorfield
+tildec = -2;
+gammaA = -0.1;
+gammaB = 0.1;
+Tend = 25;
+
+% X = (rA, rB, phiA, phiB)
+vecfield = @(~,X) [-X(1) + X(1).^3 - gammaA*X(1).*X(2).^2;...
+                    (1/tildec)*(X(2) - X(2).^3 + gammaB*X(2).*X(1).^2);...
+                    -omegaA + gamma1i*X(1).^2 + gamma2i*X(2).^2;...
+                    (1/tildec)*(-omegaB + gamma7i*X(1).^2 + gamma8i*X(2).^2)];
+
+timeEnd = 500;
+Xend = 50;
+
+
+% solve ode in reverse time
+Xinit = [1,1e-9,0,0];
+
+[t,X] = ode45(@(t,X) -vecfield(t,X),[-Tend,Tend],Xinit);
+% figure;
+% plot(X(:,1),X(:,2));
+
+% reverse solution
+t = -flip(t);
+X = flip(X,1);
+
+% plot solution
+
+% interpolate solution to uniform grid
+xiGrid = linspace(-Tend,Tend,10000);
+rA = interp1(t,X(:,1),xiGrid);
+rB = interp1(t,X(:,2),xiGrid);
+
+PhiA = interp1(t,X(:,3),xiGrid);
+PhiB = interp1(t,X(:,4),xiGrid);
+
+% figure;
+% plot(xiGrid,rA,xiGrid,rB);
+% 
+% figure;
+% plot(xiGrid,PhiA,xiGrid,PhiB);
+% 
+% pause();
+
+% full solution
+
+eps = 0.2;
+c0 = 1;
+cp = 0.5;
+
+u = @(x,time) 2*eps*interp1(t,X(:,1),eps^2*(x-c0*time)).*cos(x+interp1(t,X(:,3),eps^2*(x-c0*time))+eps^2*omegaA*time);
+v = @(x,time) 2*eps*interp1(t,X(:,2),eps^2*(x-c0*time)).*cos(x-cp*time+interp1(t,X(:,4),eps^2*(x-c0*time))+eps^2*omegaB*time);
+
+
+xgrid = linspace(0,Xend,1000);
+tgrid = linspace(0,timeEnd,1000);
+
+% video
+if makevideo
+    for ii = 1:numel(tgrid)
+        subplot(2,1,1);
+        plot(xgrid,u(xgrid,tgrid(ii)));
+        title("$u$","Interpreter","latex");
+        xlim([0,50]);
+        ylim([-0.5,0.5]);
+    
+        subplot(2,1,2);
+        plot(xgrid,v(xgrid,tgrid(ii)));
+        title("$v$","Interpreter","latex");
+        xlim([0,Xend]);
+        ylim([-0.5,0.5]);
+        pause(0.0001)
+    end
+end
+
+
+
+% space-time plot (top-down view)
+
+figure
+[Xmesh,T] = meshgrid(linspace(0,Xend,1000),linspace(0,timeEnd,1000));
+U = u(Xmesh,T);
+V = v(Xmesh,T);
+
+hold on
+s = pcolor(Xmesh,T,U);
+grid off;
+s.EdgeColor="none";
+colormap(flipud(gray))
+xlabel("$\tilde{\xi}$","FontSize",20,"Interpreter","latex")
+ylabel("$t$","FontSize",20,"Interpreter","latex")
+title("$u$","FontSize",20,"Interpreter","latex")
+xlim([0,Xend])
+ylim([0,timeEnd])
+pbaspect([4/2 1 1])
+
+set(gcf,"Position",[100, 100, 800, 420])
+
+saveas(gcf,"u-Superpos-to-T.png")
+
+figure
+
+hold on
+s = pcolor(Xmesh,T,V);
+grid off;
+s.EdgeColor="none";
+colormap(flipud(gray))
+xlabel("$\tilde{\xi}$","FontSize",20,"Interpreter","latex")
+ylabel("$t$","FontSize",20,"Interpreter","latex")
+title("$v$","FontSize",20,"Interpreter","latex")
+xlim([0,Xend])
+ylim([0,timeEnd])
+pbaspect([4/2 1 1])
+
+set(gcf,"Position",[100, 100, 800, 420])
+
+saveas(gcf,"v-Superpos-to-T.png")
